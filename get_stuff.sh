c53b7f2550531c164be7916725e5d48e8d7d5602
David Reynolds <david@reynoldsfamily.org.uk>
2010-10-08 23:49:30 +0100

    Update shell script to check for length and return N/A string if nothing is returned

--- get_stuff.sh
+++ get_stuff.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-ioreg -l | grep '"BatteryPercent" =' | sed -e 's/.*BatteryPercent" = //; s/$/%/'
+PERCENT=$(ioreg -l | grep '"BatteryPercent" =' | sed -e 's/.*BatteryPercent" = //; s/$/%/')
+if [ "${#PERCENT}" -eq 0 ];
+then
+    echo "N/A"
+else
+    echo $PERCENT
+fi
